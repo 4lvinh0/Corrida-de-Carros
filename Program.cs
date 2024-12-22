7eefb1b28fa92c64f4edb7b8d69b257e2012aba0
4lvinh0 <edunascimentoalvess@gmail.com>
2024-12-21 23:00:42 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Corrida_de_Onibus;
+
+int carro1 = 1;
+int carro2 = 1;
+
+bool andamento = true;
+
+Random rdm = new Random();
+
+
+do{
+
+#region Imagem
+Console.WriteLine(Visual.linhaExtremidade);
+
+Console.WriteLine(new string(' ', carro1 ) + Visual.topoCarro);
+Console.WriteLine(new string(' ', carro1 ) + Visual.meioCarro);
+Console.WriteLine(new string(' ', carro1 ) + Visual.baixoCarro);
+
+Console.WriteLine(Visual.linhaMeio);
+
+Console.WriteLine(new string(' ', carro2 ) + Visual.topoCarro);
+Console.WriteLine(new string(' ', carro2 ) + Visual.meioCarro);
+Console.WriteLine(new string(' ', carro2 ) + Visual.baixoCarro);
+
+Console.WriteLine(Visual.linhaExtremidade);
+
+Console.ReadKey();
+Console.Clear();
+
+#endregion
+
+#region Sorteio
+
+carro1 += rdm.Next(0, 5);
+carro2 += rdm.Next(0, 5);
+
+
+
+#endregion
+
+}while(andamento);
+
+
---
+++ Visual.cs
@@ -0,0 +1,11 @@
+﻿namespace Corrida_de_Onibus;
+
+public class Visual
+{
+    public static string linhaExtremidade = "============================================================================================================================================";
+    public static string linhaMeio = "--------------------------------------------------------------------------------------------------------------------------------------------";
+
+    public static string topoCarro = " ____";
+    public static string meioCarro = @"|_||_\";
+    public static string baixoCarro = "(o____o)";
+}
